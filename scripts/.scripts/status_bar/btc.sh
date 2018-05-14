5a69b97399a5724964d8503deb762fd7a8af216f
maciejspychala <mac.spychala@gmail.com>
2018-05-14 12:49:54 +0200

    silent curl

--- scripts/.scripts/status_bar/btc.sh
+++ scripts/.scripts/status_bar/btc.sh
@@ -1,10 +1,10 @@
-resp=$(curl https://api.coinmarketcap.com/v2/ticker/1/?convert=USD)
+resp=$(curl -s https://api.coinmarketcap.com/v2/ticker/1/?convert=USD)
 echo "${resp}" | grep price | sed 's/[^0-9.]*//g' | awk '{ printf("Ƀ %.2f$", $1);}'
 procent=$(echo "${resp}" | grep percent_change_24h | sed 's/24h//g' | sed 's/[^0-9.-]*//g')
 printf " %.2f%%\n" $procent
 echo
 if [[ $procent == -* ]] ; then
-    echo \#FF3333
+    echo \#FF2222
 else
     echo \#33FF33
 fi
